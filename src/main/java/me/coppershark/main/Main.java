70e42c240c88dbd6c7d576c5c5e59a3fb11257dc
John Doe <doej1367@gmail.com>
2022-09-21 21:13:21 +0200

    updated version number

--- src/main/java/me/coppershark/main/Main.java
+++ src/main/java/me/coppershark/main/Main.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "coppershark";
-	public static final String VERSION = "3.0.4";
+	public static final String VERSION = "3.0.5";
 
 	private byte[] tokenBadConnection = { 107, 67, 90, 100, 122, 51, 77, 52, 55, 53, 118, 49, 97, 45, 84, 55, 66, 107,
 			83, 85, 56, 120, 117, 102, 107, 81, 117, 90, 87, 110, 78, 118, 86, 88, 85, 99, 90, 76, 53, 110, 121, 80,
